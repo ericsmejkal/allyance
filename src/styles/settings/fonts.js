a8f0035efd9248f6e85438fe994d2fed0cc59499
Ashwin Vaswani <ashwin.vaswani@sweetgreen.com>
2020-06-07 13:39:55 -0700

    fix fonts

--- src/styles/settings/fonts.js
+++ src/styles/settings/fonts.js
@@ -3,11 +3,9 @@ import { css } from '@emotion/core'
 export const Fonts = css`
   @font-face {
     font-family: 'Circular Std';
-    src: url('CircularStd-Black.eot');
     src: local('Circular Std Black'), local('CircularStd-Black'),
-      url('CircularStd-Black.eot?#iefix') format('embedded-opentype'),
-      url('CircularStd-Black.woff2') format('woff2'), url('CircularStd-Black.woff') format('woff'),
-      url('CircularStd-Black.ttf') format('truetype');
+      url('../../assets/fonts/CircularStd-Black.woff2') format('woff2'),
+      url('../../assets/fonts/CircularStd-Black.woff') format('woff');
     font-weight: 900;
     font-style: normal;
     font-display: swap;
@@ -15,11 +13,9 @@ export const Fonts = css`
 
   @font-face {
     font-family: 'Circular Std Book';
-    src: url('CircularStd-Book.eot');
     src: local('Circular Std Book'), local('CircularStd-Book'),
-      url('CircularStd-Book.eot?#iefix') format('embedded-opentype'),
-      url('CircularStd-Book.woff2') format('woff2'), url('CircularStd-Book.woff') format('woff'),
-      url('CircularStd-Book.ttf') format('truetype');
+      url('../../assets/fonts/CircularStd-Book.woff2') format('woff2'),
+      url('../../assets/fonts/CircularStd-Book.woff') format('woff');
     font-weight: normal;
     font-style: normal;
     font-display: swap;
