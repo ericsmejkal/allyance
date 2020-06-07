909c06c0e9d61479f5547725e81b832b94b10fe1
Eric Smejkal <eric@philosophie.is>
2020-06-07 13:26:58 -0700

    Add bg image

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -13,7 +13,7 @@ const AtomicPageContainer = styled.main`
   align-items: center;
   position: relative;
   justify-content: flex-start;
-  z-index: 1;
+  z-index: 3;
 
   H1 {
     color: #fff;
@@ -32,9 +32,18 @@ const CustomOverlay = styled.div`
   background: linear-gradient(180deg, #030c07 0%, #2d2d2d 100%);
   height: 100vh;
   width: 100%;
-  display: block;
   position: absolute;
+  opacity: 0.93;
   z-index: 2;
+  top: 0;
+  left: 0;
+`
+
+const CustomBackground = styled.div`
+  height: 100vh;
+  width: 100%;
+  position: absolute;
+  z-index: 1;
   top: 0;
   left: 0;
 `
@@ -44,47 +53,48 @@ const AtomicPageParagraph = styled(Paragraph)`
   max-width: 600px;
   margin: 40px auto 60px 0;
   text-align: left;
+  z-index: 3;
 `
 
 export function LandingPage(props) {
   return (
     <main className="atomic-page">
       <AtomicPageContainer>
-        <CustomOverlay>
-          <Flex
-            width={1}
-            height="100vh"
-            alignItems="center"
-            pl={[4, 4, 6, 6]}
-            pr={[4, 4, 6, 6]}
-            style={{ backgroundImage: `url(${backgroundImage})` }}
-          >
-            <Box width={1}>
-              <H3>Strength in numbers.</H3>
-              <H1>
-                Holding our justice system
-                <br />
-                accountable for acts of
-                <br />
-                <ReactRotatingText
-                  items={[
-                    'police brutality.',
-                    'excessive force.',
-                    'white supremacy.',
-                    'social injustice.',
-                  ]}
-                  cursor={false}
-                />
-              </H1>
-              <AtomicPageParagraph largeText>
-                If you would like to join the fight, sign up below and help in our quest to develop
-                a platform to hold our police force accountable for their actions, in an effort to
-                influence the needed change we deserve.
-              </AtomicPageParagraph>
-              <MailchimpForm />
-            </Box>
-          </Flex>
-        </CustomOverlay>
+        <Flex
+          height="100vh"
+          style={{ zIndex: '4' }}
+          alignItems="center"
+          pl={[4, 4, 6, 6]}
+          pr={[4, 4, 6, 6]}
+        >
+          <Box>
+            <H3>Strength in numbers.</H3>
+            <H1>
+              Holding our justice system
+              <br />
+              accountable for acts of
+              <br />
+              <ReactRotatingText
+                items={[
+                  'police brutality.',
+                  'excessive force.',
+                  'white supremacy.',
+                  'social injustice.',
+                ]}
+                cursor={false}
+              />
+            </H1>
+            <AtomicPageParagraph largeText>
+              If you would like to join the fight, sign up below and help in our quest to develop a
+              platform to hold our police force accountable for their actions, in an effort to
+              influence the needed change we deserve.
+            </AtomicPageParagraph>
+            <MailchimpForm />
+          </Box>
+        </Flex>
+
+        <CustomOverlay />
+        <CustomBackground style={{ backgroundImage: `url(${backgroundImage})` }} />
       </AtomicPageContainer>
     </main>
   )
